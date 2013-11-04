730a5ef81fe4f73adbd43d265f2313d4e1793e08
johnschultz <jomoschultz@gmail.com>
2013-11-03 21:24:00 -0800

    Contacts now added to list when selected

--- gen/sf/kit/R.java
+++ gen/sf/kit/R.java
@@ -24,11 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
-        public static final int add_contact=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int add_contact=0x7f080005;
+        public static final int contact_item_name=0x7f080002;
+        public static final int contact_list_item=0x7f080001;
+        public static final int contact_reminder_time=0x7f080003;
+        public static final int reminders_list=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int reminder_list_view_item=0x7f030001;
     }
     public static final class menu {
         public static final int add_contact=0x7f070000;
