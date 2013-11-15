8dcbea407b09919662e1d9ea0126db50f781036b
Johnathon Schultz <jschultz@yelp.com>
2013-11-14 19:12:13 -0800

    Added functionality for saving contacts on app close

--- src/sf/kit/MainActivity.java
+++ src/sf/kit/MainActivity.java
@@ -1,5 +1,12 @@
 package sf.kit;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -8,7 +15,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.Contacts;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -25,25 +31,30 @@ public class MainActivity extends Activity {
 		);
 		cursor.moveToFirst();
 		Reminder newReminder = new Reminder(
-			contactUri,
 			cursor.getInt(ID_INDEX),
 			cursor.getString(LOOKUP_KEY_INDEX),
 			cursor.getString(NAME_INDEX)
 		);
-		adapter.add(newReminder);
+		remindersListAdapter.add(newReminder);
 	}
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        reminders = new ArrayList<Reminder>();
+        remindersList = new ArrayList<Reminder>();
         remindersListView = (ListView) findViewById(R.id.reminders_list);
-        adapter = new MyArrayAdapter(this, reminders);
-        remindersListView.setAdapter(adapter);
+        remindersListAdapter = new RemindersArrayAdapter(this, remindersList);
+        remindersListView.setAdapter(remindersListAdapter);
+        loadRemindersList();
     }
 
-
+    @Override
+    protected void onStop(){
+    	super.onStop();
+    	saveRemindersList();
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -56,6 +67,9 @@ public class MainActivity extends Activity {
     	case R.id.add_contact:
     		addContactButtonClicked();
     		return true;
+    	case R.id.clear_reminders:
+    		clearRemindersButtonClicked();
+    		return true;
 		default:
     		return super.onOptionsItemSelected(item);
     	}
@@ -64,6 +78,10 @@ public class MainActivity extends Activity {
     public void addContactButtonClicked(){
     	Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
     	this.startActivityForResult(contactPickerIntent, CONTACT_PICKER_REQUEST);
+    }
+    
+    public void clearRemindersButtonClicked(){
+    	remindersListAdapter.clear();
     }
     
     public void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -77,13 +95,61 @@ public class MainActivity extends Activity {
     	}
     }
     
+    private void loadRemindersList(){
+    	try {
+    		System.out.print("Loading file...");
+			FileInputStream inStream = openFileInput(REMINDERS_FILE_NAME);
+			ObjectInputStream objIn = new ObjectInputStream(inStream);
+
+			ArrayList<Reminder> loadedList = (ArrayList<Reminder>) objIn.readObject();
+			if(loadedList != null){
+				remindersList.clear();
+				remindersList.addAll(loadedList);
+    		}
+			remindersListAdapter.notifyDataSetChanged();
+			objIn.close();
+			inStream.close();
+			System.out.println(" done.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("File not found when loading saved reminders.");
+		} catch (StreamCorruptedException e) {
+			e.printStackTrace();
+			System.err.println("Stream corrupted when loading saved reminders.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("IOException when loading saved reminders.");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Class not found when loading saved reminders.");
+		}
+    }
+    
+    private void saveRemindersList(){
+    	try {
+    		System.out.print("Saving file...");
+    		FileOutputStream outStream = openFileOutput(REMINDERS_FILE_NAME, MODE_PRIVATE);
+			ObjectOutputStream objOut = new ObjectOutputStream(outStream);
+			objOut.writeObject(remindersList);
+			objOut.close();
+			outStream.close();
+			System.out.println(" done");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("File not found when loading saved reminders.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("IOException when loading saved reminders.");
+		}
+    }
+    
     private static final int CONTACT_PICKER_REQUEST = 1;
     
-    private ArrayList<Reminder> reminders;
+    private ArrayList<Reminder> remindersList;
     
     private ListView remindersListView;
     
-    private MyArrayAdapter adapter;
+    private RemindersArrayAdapter remindersListAdapter;
     
     private static final String[] PROJECTION = {
     	ContactsContract.Contacts._ID,
@@ -94,4 +160,6 @@ public class MainActivity extends Activity {
     private static final String SELECTION = ContactsContract.Contacts._ID + "=?";
     
     private static final int ID_INDEX = 0, LOOKUP_KEY_INDEX = 1, NAME_INDEX = 2;
+    
+    private static final String REMINDERS_FILE_NAME = "contacts_to_remind.txt";
 }
--- src/sf/kit/Reminder.java
+++ src/sf/kit/Reminder.java
@@ -1,32 +1,26 @@
 package sf.kit;
 
-import android.net.Uri;
-import android.provider.ContactsContract;
+import java.io.Serializable;
 
-public class Reminder {
+
+public class Reminder implements Serializable{
 
 	private String name;
-	private Uri uri;
 	private int id;
 	private String lookupKey;
 	private int reminderTime;
 	
 	private static final int DEFAULT_REMINDER_TIME = 42;
 	
-	public Reminder(Uri uri, int id, String lookupKey, String name){
+	public Reminder(int id, String lookupKey, String name){
 		this.name = name;
 		this.id = id;
 		this.lookupKey = lookupKey;
 		this.reminderTime = DEFAULT_REMINDER_TIME;
-		this.uri = uri;
 	}
 	
 	public String getName() {
 		return name;
-	}
-
-	public Uri getUri() {
-		return uri;
 	}
 
 	public int getId() {
@@ -40,4 +34,6 @@ public class Reminder {
 	public String getLookupKey() {
 		return lookupKey;
 	}
+	
+	private static final long serialVersionUID = 7476211309892789793L;
 }
